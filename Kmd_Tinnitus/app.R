21a6dea39292c984b91b72307a8230987375c05c
Priyanka-M96 <priyankamohan455@gmail.com>
2019-08-23 17:15:03 +0200

    Update Proclus and Shiny app

--- Kmd_Tinnitus/app.R
+++ Kmd_Tinnitus/app.R
@@ -14,9 +14,11 @@ ui <- fluidPage(
     pageWithSidebar(
         headerPanel('Tinnitus Analysis'),
         sidebarPanel(
-            selectInput('clust', 'CLustering Algorithm',choices = list("kmeans" = 1,"Heirarchichal" = 2,"Hkmeans" = 3),selected = 1),
-            selectInput('visual','Type of visualization',choices = list("Cluster" = 1,"Decision Tree" = 2,"Radial Chart" = 3)),
-            selectInput('clusters', 'Cluster count',choices = list("2" = 2,"4" = 4),selected = 1),
+            selectInput('Algorithm', 'CLustering Algorithm',
+                        choices = list("Select an algorithm" = 0,"kmeans" = 1,"Heirarchichal" = 2,"Hkmeans" = 3,"PC kmeans" = 4,"Proclus" = 5,"Orclus" = 6),selected = 0),
+            selectInput('visual','Type of visualization',choices = list("Select" = 0,"Decision Tree" = 1,"Radial Chart" = 2),selected = 0),
+            numericInput('clusters', 'Cluster count',3,min = 2,max = 9),
+            actionButton("update", "Show"),
             hr(),
             fluidRow(column(3, verbatimTextOutput("value")))
         ),
@@ -30,24 +32,26 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  condition <- reactive(input$visual)
+  alg <- reactive(input$Algorithm)
+  
   selectedData <- reactive({
-    df_noCorr_scaled
+    clust(input$clusters)
   })
   
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
-  })
+  view <- reactive(if( condition() == 1 ) 
+                    DT(selectedData)
+                  else 
+                    radial(selectedData)
+                    )
   
-  output$plot1 <- renderPlot({
-    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
-    
-    par(mar = c(1,1,1,1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
-  })
+
+  
+  clust <- reactive(
+    if(alg() == "kmeans")
+      
+    )
+  
   
 }
 
