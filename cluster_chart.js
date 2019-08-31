d6e409f245bfabee539047803c125b12f5cdecfc
Kritika Bhowmik <kritika.bhowmik@gmail.com>
2019-08-31 18:35:07 +0200

    changed scale for chart to 1.5

--- cluster_chart.js
+++ cluster_chart.js
@@ -18,14 +18,14 @@ var algo= data.filter(function(d,i) { if (i===1) {return d.algorithm;} });
 
 var y = d3.scaleLinear()
       .range([innerRadius, outerRadius])   // Domain will be define later.
-      .domain([0,2]); // Domain of Y is from 0 to the max seen in the data
+      .domain([0,1.5]); // Domain of Y is from 0 to the max seen in the data
       
 var scaley = d3.scaleLinear()
       .range([innerRadius-(outerRadius-innerRadius), outerRadius])   // Domain will be define later.
-      .domain([-2,2 ]); // Domain of Y is from 0 to the max seen in the data
+      .domain([-1.5,1.5 ]); // Domain of Y is from 0 to the max seen in the data
 var xAxis = d3.axisLeft()
       .scale(scaley)
-      .ticks(5);
+      .ticks(7);
       
 var tooltip = d3.select("body")
       .append("div")
@@ -72,7 +72,7 @@ svg=svg.append("g")
     .attr("class", "x axis")
     .call(xAxis);
 var circles = svg.selectAll("circle")
-          .data([-2,-1.5,-1,-0.5,0,0.5,1,1.5,2])
+          .data([-1.5,-1,-0.5,0,0.5,1,1.5])
           .enter().append("circle")
           .attr("r", function(d) {return scaley(d);})
           .style("fill", "none")
